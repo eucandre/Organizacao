fbd233bca7e45d6e8c8dd0f87b5449ee168920da
eucandre <eucandre@gmail.com>
2017-12-25 13:54:25 -0200

    criado forms do app_clientes

--- Organiza/app_clientes/views.py
+++ Organiza/app_clientes/views.py
@@ -8,3 +8,14 @@ def Cria_segmento(request):
 	else:
 		form = FormSegmento()
 	return render(request,"Cria_segmento.html",{"form":form})
+
+
+def Cria_Cliente(request):
+	if request.method == 'POST':
+		form = FormCliente(request.POST)
+		if form.is_valid():
+			form.save()
+	else:
+		form = FormCliente()
+	return render(request,"Cria_cliente.html",{"form":form})
+
